5cb2f14a0dd8fc5fa5107ec2d742f0afcdac6193
Ariz-99 <arizmuhammadfajar@gmail.com>
2021-04-18 21:35:11 +0700

    Update tugas_modul4.cpp

--- tugas_modul4.cpp
+++ tugas_modul4.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void garis() {
+	for (int i = 0; i < 25; i++) {
+		cout << "=";
+	}
+}
+
 int main() {
-	return 0;
+	garis();
+	cout << endl;
+	cout << "Kelompok 20" << endl;
+	garis();
+	cout << endl;
+	cout << "A.Miftachul Hidayat" << endl;
+	cout << "Aldo Serena Safiola" << endl;
+	cout << "Ariz Muhammad Fajar" << endl;
+	cout << "Putri Almaas Auliasari" << endl << endl;
 }
