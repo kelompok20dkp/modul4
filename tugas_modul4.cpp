d5eb7f9c89f2e1ef830b3c73cc2f47186699e73b
Putrialmaas <PutriAlmaas09@gmail.com>
2021-04-18 21:51:12 +0700

    default

--- tugas_modul4.cpp
+++ tugas_modul4.cpp
@@ -8,6 +8,10 @@ void garis() {
 }
 
 int main() {
+	int angka;
+	float jumlah;
+	char pilihan;
+	pilihan = 'y';
 	garis();
 	cout << endl;
 	cout << "Kelompok 20" << endl;
@@ -17,4 +21,17 @@ int main() {
 	cout << "Aldo Serena Safiola" << endl;
 	cout << "Ariz Muhammad Fajar" << endl;
 	cout << "Putri Almaas Auliasari" << endl << endl;
+	while (pilihan == 'y') {
+		cout << "Selamat datang di program konversi nilai Rupiah sederhana" << endl;
+		for (int i = 0; i < 57; i++) {
+			cout << "-";
+		}
+		cout << endl;
+		cout << "Pilih konversi" << endl;
+		cout << "1. Rupiah => Dolar Amerika" << endl;
+		cout << "2. Rupiah => Euro" << endl;
+		cout << "3. Rupiah => Yen" << endl;
+		cout << "Masukkan input: ";
+		cin >> angka;
+	}
 }
